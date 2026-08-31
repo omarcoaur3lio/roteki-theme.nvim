9baface2286f0d56b8394699a261845b64a9f4cb
Marco Aurélio <sgtmarcoaurelio@icloud.com>
2026-08-30 21:12:54 -0300

    chore: add gitignore and test harness, drop stray files

---
+++ tests/init.lua
@@ -0,0 +1,8 @@
+local root = vim.fn.fnamemodify(".", ":p")
+
+vim.opt.runtimepath:append(root)
+
+local plenary_path = root .. "pack/vendor/start/plenary.nvim"
+if vim.uv.fs_stat(plenary_path) then
+  vim.opt.runtimepath:append(plenary_path)
+end
